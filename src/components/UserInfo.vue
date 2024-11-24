50f71815caec2ccb96cc7a1c5d63f6f359e2524d
Najib <najib.jodian@mekari.com>
2024-11-24 23:36:39 +0700

    vue propps to pass values to child component

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import Card from 'primevue/card'
+import type { User } from '@/types/user'
+import { PropType } from 'vue'
+
+const props = defineProps({
+  user: {
+    type: Object as PropType<User>,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <Card>
+    <template #tittle>User Profile</template>
+    <template #content>
+      <div class="flex flex-column rom-gap-2">
+        <div>
+          <i class="pi pi-user" />
+          <span class="m-2">{{ user.full_name }}</span>
+        </div>
+
+        <div>
+          <i class="pi pi-envelope" />
+          <span class="m-2">
+            <a :href="`mailto:${user.email}`"> { user.email } </a>
+          </span>
+        </div>
+      </div>
+    </template>
+  </Card>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import Toast from 'primevue/toast'
 import LoginUser from '../components/LoginUser.vue'
+import UserInfo from '@/components/UserInfo.vue'
+import store from '../store'
 </script>
 
 <template>
   <main>
     <Toast></Toast>
     <h1 class="green">Welcome to simple bank!</h1>
-    <LoginUser></LoginUser>
+    <UserInfo v-if="store.state.user" :user="store.state.user" />
+    <LoginUser v-else />
   </main>
 </template>
