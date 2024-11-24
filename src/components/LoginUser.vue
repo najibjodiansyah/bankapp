899f23d45dfbb3d9ba9ccf5ef959fa5d863d070c
Najib <najib.jodian@mekari.com>
2024-11-24 22:27:22 +0700

    build login form using primevue and primeflex

---
+++ src/components/LoginUser.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import InputGroup from 'primevue/inputgroup'
+import InputGroupAddon from 'primevue/inputgroupaddon'
+import InputText from 'primevue/inputtext'
+</script>
+
+<template>
+  <div class="flex flex-column row-gap-3">
+    <InputGroup>
+      <InputGroupAddon>
+        <i class="pi pi-user"></i>
+      </InputGroupAddon>
+      <InputText placeholder="Username" />
+    </InputGroup>
+
+    <InputGroup>
+      <InputGroupAddon>
+        <i class="pi pi-lock"></i>
+      </InputGroupAddon>
+      <InputText type="password" placeholder="Password" />
+    </InputGroup>
+  </div>
+</template>
